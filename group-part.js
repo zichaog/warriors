b0fe80684dc8b66c804604482df6ec084aad494d
keechaw <106809859+zichaog@users.noreply.github.com>
2022-07-12 17:17:51 +0800

    Delete group-part.js

--- group-part.js
+++
@@ -1,8 +0,0 @@
-/*球队分区切换板块*/
-
-function $(id){return document.getElementById(id);}/*获取元素*/
-function changeImage(){
-	var o=$("group");
-	var index=o.selectedIndex;//获取下拉框中的选择项
-	$("show").src=$("group").options[index].value;  //更改图片
-}
